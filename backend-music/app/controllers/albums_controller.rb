20646619a2416268b6898fbfc55e5b7cc2b8bbee
djsteve33 <swomble@aol.com>
2021-07-26 15:44:07 -0400

    added show and index actions to Albums and Genres controllers

--- backend-music/app/controllers/albums_controller.rb
+++ backend-music/app/controllers/albums_controller.rb
@@ -1,2 +1,12 @@
 class AlbumsController < ApplicationController
+
+    def index
+        albums = Album.all
+        render json: albums
+    end
+
+    def show
+        album = Album.find(params{:id})
+        render json: album
+    end
 end
