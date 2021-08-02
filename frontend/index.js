ae7cc9a2ee776f19b8afe61fcf871bad1addfcb3
Steve Womble <swomble@aol.com>
2021-08-01 20:46:05 -0400

    added variables and DOMContentLoaded to index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,18 @@
-const albumList = document.getElementById('album-list')
+const albumForm = document.getElementById('album-form')
+const albumsAdapter = new AlbumsAdapter
+const genresAdapter = new genresAdapter
+const newFormButton = document.getElementById('new-form-btn')
 
-function fetchAlbums() {
-    
+function hideBtnLoadForm(e) {
+    e.target.hidden = true
+    const newForm = document.getElementById('new-form-container')
+    newForm.hidden = false
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetchAlbums()
+    albumsAdapter.fetchAlbums()
+    genresAdapter.fetchGenres()
+    albumForm.addEventListener('submit', albumsAdapter.handleFormSubmit)
+    newFormButton.addEventListener('click', hideBtnLoadForm)
+    debugger
 })
